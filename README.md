56d41bcbb45def53480cb4025ca4dcc5836d835f
Alan Knight <alanknight@google.com>
2014-09-15 10:45:42 -0700

    Comments and create an initial version for publishing

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-This is an example of a minimal transformer that generates CustomRule subclasses
-for serialization of Dart struct objects (only public fields, no constructor parameters).
+This provides a minimal transformer that generates CustomRule subclasses
+for serialization of Dart struct objects (only public fields required
+for serialization, no constructor parameters).
+
+For an example of usage, see 
+http://www.dartdocs.org/documentation/minimal_serialization_example/latest
+
+Basic usage is 
+In your pubpsec
+      transformers:
+        - minimal_serialization :
+          $include: lib/stuff.dart lib/more_stuff.dart
+ For each library 'foo' listed in the $include section this will
+ generate a 'foo_serialization_rules.dart' library with serialization
+ rules for those classes. You can use these like
+       import 'package:my_package/stuff_serialization_rules.dart' as foo;
+       ...
+       var serialization = new Serialization();
+       foo.rules.values.forEach(serialization.addRule);
--- lib/transformer.dart
+++ lib/transformer.dart
@@ -8,6 +8,19 @@ import "package:barback/barback.dart";
 import "package:analyzer/analyzer.dart";
 import "package:path/path.dart" as path;
 
+/// A transformer for generating serialization rules. Usage is as follows.
+/// In your pubpsec
+///      transformers:
+///        - minimal_serialization :
+///          $include: lib/stuff.dart lib/more_stuff.dart
+/// For each library 'foo' listed in the $include section this will
+/// generate a 'foo_serialization_rules.dart' library with serialization
+/// rules for those classes. You can use these like
+///       import 'package:my_package/stuff_serialization_rules.dart' as foo;
+///       ...
+///       var serialization = new Serialization();
+///       foo.rules.values.forEach(serialization.addRule);
+/// For an example, see package minimal_serialization_example
 class MinimalSerializationTransformer extends Transformer {
 
   get allowedExtensions => ".dart";
@@ -29,7 +42,8 @@ class MinimalSerializationTransformer extends Transformer {
         // Remove the leading /lib on the file name.
         var fileNameInPackage = path.joinAll(path.split(id.path)..removeAt(0));
         var text = '''
-// Generated serialization rules
+// Generated serialization rules. *** DO NOT EDIT ***
+// See package minimal_serialization.
 library ${path.basenameWithoutExtension(fileName)}_serialization_rules.dart;
 
 import "package:serialization/serialization.dart";
@@ -49,6 +63,9 @@ ${rules.map((x) => x.rule).join("\n\n")}
 
 }
 
+/// Generates serialization rules for a class.
+// TODO(alanknight): Generalize to be able to to handle more complex
+// cases similarly to BasicRule in package:serialization.
 class CustomRuleGenerator {
   ClassDeclaration declaration;
   CustomRuleGenerator(this.declaration);
